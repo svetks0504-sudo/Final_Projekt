d99ce8b4fb2897129e220819ca76f7f361fd4cce
Svitlana <svetks0504@gmail.com>
2026-01-12 00:36:03 +0100

    started a second page

--- scripts/script.js
+++ scripts/script.js
@@ -1,105 +1,3 @@
-const eventsNear = [
-{
-  image:"assets/images/cart1.svg",
-  title:"Day Trading Idea and Strategy",
-  category: "Business (5 km)",
-  date:"MON, MAR 18 · 7:00 PM PDT",
- attendees: "1 going",
-    price: "Free"
-},
-{
-  image:"assets/images/cart2.svg",
-  title:"Let's Talk Networking: JPMorgan Chase in Palo Alto",
-  category:"Business (25 km)",
-  date:"TUE, MAR 19 · 5:00 PM PDT",
- attendees: "41 going",
-    price: "Free"
-},
-{
-  image:"assets/images/cart3.svg",
-  title:"Tech Talks & Quiz: Next-Gen Database Solutions for Emerging Use Cases",
-  category:"Technology",
-  date:"WED, MAR 13 · 6:00 PM PDT",
- attendees: "40 going",
-    price: "Free"
-},
-{
-  image:"assets/images/cart4.svg",
-  title:"INFORMS San Francisco Chapter In-Person Event",
-  category:"Health and Wellbeing (50 km)",
-  date:"THU, MAR 28 · 5:00 PM PDT",
- attendees: "41 going",
-    price: "Free"
-},
-{
-  image:"assets/images/cart5.svg",
-  title:"AI Wednesdays - Meet and Greet!",
-  category:"Technology (5 km)",
-  date:"WED, MAR 13 · 6:30 PM PDT",
- attendees: "29 going",
-    price: "Free"
-},
-{
-  image:"assets/images/cart6.svg",
-  title:"ROS By-The-Bay March 2024",
-  category:"Social Activities",
-  date:"THU, MAR 21 · 6:00 PM PDT",
- attendees: "51 going",
-    price: "Free"
-},
-{
-  image:"assets/images/cart7.svg",
-  title:"Free Christian Singles' Dinner",
-  category:"Hobbies and Passions (10 km)",
-  date:"FRI, MAR 29 · 6:00 PM PDT",
- attendees: "11 going",
-    price: "Free"
-},
-{
-  image:"assets/images/cart8.svg",
-  title:"In-person: Deep Dive into RAG Architectures (Food served)",
-  category:"Hobbies and Passions (50 km)",
-  date:"THU, MAR 14 · 5:00 PM PDT",
- attendees: "16 going",
-    price: "Free"
-},
-];
-
-const upcomingOnlineEvents = [
-{
-  image:"assets/images/cart9.svg",
-  title:"Amazing On-Demand 15 Min Interviews with Top Coaches and Speakers",
-  category:"Business (25 km)",
-  date:"THU, MAR 14 · 6:00 PM PDT",
- attendees: "3 going",
-    price: "Free"
-},
-{
-  image:"assets/images/cart10.svg",
-  title:"Vision Pro Developers Online Meetup",
-  category:"Technology",
-  date:"WED, MAR 13 · 7:00 PM PDT",
- attendees: "51 going",
-    price: "Free"
-},
-{
-  image:"assets/images/cart11.svg",
-  title:"Significant Musical Moments",
-  category:"Hobbies and Passions (10 km)",
-  date:"WED, MAR 13 · 6:00 PM PDT",
- attendees: "16 going",
-    price: "Free"
-},
-{
-  image:"assets/images/cart12.svg",
-  title:"FREE Webinar: Introduction to Power BI",
-  category:"Technology",
-  date:"THU, MAR 14 · 5:30 PM PDT",
- attendees: "33 going",
-    price: "Free"
-},
-];
-
 const eventsStore = [
   {
     title: "INFJ Personality Type - Coffee Shop Meet & Greet",
@@ -202,12 +100,125 @@ const filters = [
 ];
 
 
+const eventsNear = [
+{
+  image:"assets/images/cart1.svg",
+  title:"Day Trading Idea and Strategy",
+  category: "Business (5 km)",
+  date:"MON, MAR 18 · 7:00 PM PDT",
+ attendees: "1 going",
+    price: "Free"
+},
+{
+  image:"assets/images/cart2.svg",
+  title:"Let's Talk Networking: JPMorgan Chase in Palo Alto",
+  category:"Business (25 km)",
+  date:"TUE, MAR 19 · 5:00 PM PDT",
+ attendees: "41 going",
+    price: "Free"
+},
+{
+  image:"assets/images/cart3.svg",
+  title:"Tech Talks & Quiz: Next-Gen Database Solutions for Emerging Use Cases",
+  category:"Technology",
+  date:"WED, MAR 13 · 6:00 PM PDT",
+ attendees: "40 going",
+    price: "Free"
+},
+{
+  image:"assets/images/cart4.svg",
+  title:"INFORMS San Francisco Chapter In-Person Event",
+  category:"Health and Wellbeing (50 km)",
+  date:"THU, MAR 28 · 5:00 PM PDT",
+ attendees: "41 going",
+    price: "Free"
+},
+{
+  image:"assets/images/cart5.svg",
+  title:"AI Wednesdays - Meet and Greet!",
+  category:"Technology (5 km)",
+  date:"WED, MAR 13 · 6:30 PM PDT",
+ attendees: "29 going",
+    price: "Free"
+},
+{
+  image:"assets/images/cart6.svg",
+  title:"ROS By-The-Bay March 2024",
+  category:"Social Activities",
+  date:"THU, MAR 21 · 6:00 PM PDT",
+ attendees: "51 going",
+    price: "Free"
+},
+{
+  image:"assets/images/cart7.svg",
+  title:"Free Christian Singles' Dinner",
+  category:"Hobbies and Passions (10 km)",
+  date:"FRI, MAR 29 · 6:00 PM PDT",
+ attendees: "11 going",
+    price: "Free"
+},
+{
+  image:"assets/images/cart8.svg",
+  title:"In-person: Deep Dive into RAG Architectures (Food served)",
+  category:"Hobbies and Passions (50 km)",
+  date:"THU, MAR 14 · 5:00 PM PDT",
+ attendees: "16 going",
+    price: "Free"
+},
+];
+
+const upcomingOnlineEvents = [
+{
+  image:"assets/images/cart9.svg",
+  title:"Amazing On-Demand 15 Min Interviews with Top Coaches and Speakers",
+  category:"Business (25 km)",
+  date:"THU, MAR 14 · 6:00 PM PDT",
+ attendees: "3 going",
+    price: "Free"
+},
+{
+  image:"assets/images/cart10.svg",
+  title:"Vision Pro Developers Online Meetup",
+  category:"Technology",
+  date:"WED, MAR 13 · 7:00 PM PDT",
+ attendees: "51 going",
+    price: "Free"
+},
+{
+  image:"assets/images/cart11.svg",
+  title:"Significant Musical Moments",
+  category:"Hobbies and Passions (10 km)",
+  date:"WED, MAR 13 · 6:00 PM PDT",
+ attendees: "16 going",
+    price: "Free"
+},
+{
+  image:"assets/images/cart12.svg",
+  title:"FREE Webinar: Introduction to Power BI",
+  category:"Technology",
+  date:"THU, MAR 14 · 5:30 PM PDT",
+ attendees: "33 going",
+    price: "Free"
+},
+];
+
+
 const eventsNearDiv = document.querySelector(".eventsNearDiv");
 const upcomingOnlineEventsDiv = document.querySelector(".upcomingOnlineEventsDiv");
+const imageMeetup = document.querySelector("#imageMeetup");
+const joinMeetup = document.querySelector(".joinMeetup");
 
+function pageSwitcher(elem, src){
+   if (!elem) return;
 
+  elem.addEventListener("click", ()=>{
+    window.location.href = src;
+  });
+}
 
 function createCartsForEvents(arr, container){
+  if (!container) return;
+
   arr.forEach((element)=>{
 const newCarts = document.createElement("div");
 newCarts.classList.add("carts");
@@ -244,3 +255,6 @@ calendar_today
 
 createCartsForEvents(eventsNear, eventsNearDiv);
 createCartsForEvents(upcomingOnlineEvents, upcomingOnlineEventsDiv);
+
+pageSwitcher(imageMeetup, "index.html");
+pageSwitcher(joinMeetup, "secondPage.html");
